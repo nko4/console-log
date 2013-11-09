e95c9d4cfce44aa6f48415a1070f362904d10fa8
Todd Wolfson <todd@twolfson.com>
2013-11-08 23:22:52 -0800

    Collecting image data via phantomjs

---
+++ demo/phantomjs-image-info.js
@@ -0,0 +1,26 @@
+var phantomjssmith = require('../phantomjssmith-hack');
+
+module.exports = function getImageData (image, cb) {
+  // Convert images into phantomjssmith objects
+  var images = [image];
+  phantomjssmith.createImages(images, function handleImages (err, imgs) {
+    // Create a canvas to draw onto (200 pixels wide, 300 pixels tall)
+    phantomjssmith.createCanvas(200, 200, function (err, canvas) {
+      // Add each image at a specific location (upper left corner = {x, y})
+      var coordinatesArr = [{x: 0, y: 0}, {x: 50, y: 50}];
+      imgs.forEach(function (img, i) {
+        var coordinates = coordinatesArr[i];
+        canvas.addImage(img, coordinates.x, coordinates.y);
+      }, canvas);
+
+      // Export canvas to image
+      canvas['export']({format: 'png'}, function (err, result) {
+        cb(null, result); // Binary string representing a PNG image of the canvas
+      });
+    });
+  });
+};
+
+if (module.parent === null) {
+  module.exports(__dirname + '/test1-orig.jpg', console.log);
+}
