0e3c02a9d8f63328ba3b2458bbe7f0c349fb421e
Todd Wolfson <todd@twolfson.com>
2013-11-09 12:45:31 -0800

    Moving gif-server to module.exports for uploading demo

--- demo/gif-server.js
+++ demo/gif-server.js
@@ -2,15 +2,21 @@ var http = require('http');
 var fs = require('fs');
 var gifGenerate = require('./gif-generate');
 
-var app = http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'connection': 'keep-alive',
-    'content-type': 'image/gif',
-    'transfer-encoding': 'chunked'
-  });
-  gifGenerate(res);
-});
+module.exports = function gifServer (port) {
 
-var port = 7000;
-app.listen(port);
-console.log('gifsockets server is listening at http://127.0.0.1:7000/');
+  var app = http.createServer(function (req, res) {
+    res.writeHead(200, {
+      'connection': 'keep-alive',
+      'content-type': 'image/gif',
+      'transfer-encoding': 'chunked'
+    });
+    gifGenerate(res);
+  });
+
+  app.listen(port);
+  console.log('gifsockets server is listening at http://127.0.0.1:7000/');
+};
+
+if (module.parent === null) {
+  module.exports(7000);
+}
