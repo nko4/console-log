b5bb7e62e059c910f15601bd76cbba02761c0ce6
Todd Wolfson <todd@twolfson.com>
2013-11-08 18:08:01 -0800

    Writing out content to file

--- demo/gif-generate.js
+++ demo/gif-generate.js
@@ -1,5 +1,6 @@
 // Image from http://i1.ytimg.com/vi/QH2-TGUlwu4/hqdefault.jpg
 var GifEncoder = require('gif.js/src/GIFEncoder');
+
 var Canvas = require('canvas');
 var canvas = new Canvas(200, 200);
 
@@ -15,4 +16,6 @@ ctx.lineTo(50, 102);
 ctx.lineTo(50 + te.width, 102);
 ctx.stroke();
 
-console.log('<img src="' + canvas.toDataURL() + '" />');
+// console.log('<img src="' + canvas.toDataURL() + '" />');
+var fs = require('fs');
+fs.writeFileSync(__dirname + '/test.png', canvas.toDataURL().replace('data:image/png;base64,', ''), 'base64');
