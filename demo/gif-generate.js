c9f51e33bd3b8e8fe543efc3c325476c6e738fed
Todd Wolfson <todd@twolfson.com>
2013-11-08 18:04:51 -0800

    Outputting image content

---
+++ demo/gif-generate.js
@@ -0,0 +1,18 @@
+// Image from http://i1.ytimg.com/vi/QH2-TGUlwu4/hqdefault.jpg
+var GifEncoder = require('gif.js/src/GIFEncoder');
+var Canvas = require('canvas');
+var canvas = new Canvas(200, 200);
+
+var ctx = canvas.getContext('2d');
+
+ctx.font = '30px Impact';
+ctx.fillText("Awesome!", 50, 100);
+
+var te = ctx.measureText('Awesome!');
+ctx.strokeStyle = 'rgba(0,0,0,0.5)';
+ctx.beginPath();
+ctx.lineTo(50, 102);
+ctx.lineTo(50 + te.width, 102);
+ctx.stroke();
+
+console.log('<img src="' + canvas.toDataURL() + '" />');
