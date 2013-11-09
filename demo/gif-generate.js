1554d859ab3797b4b29a41e55da191a8fe910921
Todd Wolfson <todd@twolfson.com>
2013-11-08 19:05:41 -0800

    The data looks the same ;_;

--- demo/gif-generate.js
+++ demo/gif-generate.js
@@ -28,25 +28,44 @@ var imgKeys = Object.getOwnPropertyNames(imageData).map(function (int) {
 var maxKey = imgKeys.reduce(function (a, b) {
   return Math.max(a, b);
 }, 0);
-
-// Compare to canvas data
-// https://gist.github.com/borismus/1032746
-var BASE64_MARKER = ';base64,';
+var data = new Uint8Array(maxKey);
+for (var i = 0; i < maxKey; i++) {
+  data[i] = imageData[i];
+}
 
 var atob2 = require('atob');
-function convertDataURIToBinary(dataURI) {
-  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-  var base64 = dataURI.substring(base64Index);
-  // var raw = window.atob(base64);
-  var raw = atob2(base64);
-  var rawLength = raw.length;
-  var array = new Uint8Array(new ArrayBuffer(rawLength));
+// Compare to canvas data
+// https://gist.github.com/borismus/1032746
+// var BASE64_MARKER = ';base64,';
 
-  for(i = 0; i < rawLength; i++) {
-    array[i] = raw.charCodeAt(i);
-  }
-  return array;
+// function convertDataURIToBinary(dataURI) {
+//   var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+//   var base64 = dataURI.substring(base64Index);
+//   // var raw = window.atob(base64);
+//   var raw = atob2(base64);
+//   var rawLength = raw.length;
+//   var array = new Uint8ClampedArray(new ArrayBuffer(rawLength));
+
+//   for(i = 0; i < rawLength; i++) {
+//     array[i] = raw.charCodeAt(i);
+//   }
+//   return array;
+// }
+
+// http://stackoverflow.com/questions/16194908/how-can-i-create-a-canvas-imagedata-array-from-an-arraybuffer-representation-of
+function convertDataURIToBinary(dataURI) {
+  // uri to Base64
+  b64 = dataURI.slice(dataURI.indexOf(',')+1);
+  // to String
+  str = atob2(b64);
+  // to Array
+  arr = str.split('').map(function (e) {return e.charCodeAt(0);});
+  // to Uint8ClampedArray
+  u = new Uint8ClampedArray(arr); // [255, 56, 201, 8]
+
+  return u;
 }
 console.log(convertDataURIToBinary(canvas.toDataURL()));
+// convertDataURIToBinary(canvas.toDataURL());
 
 // TODO: Output canvas data to gif
