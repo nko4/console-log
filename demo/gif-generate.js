dffd477d6213a0faf4b0e90218c01c2abdbc2bfe
Todd Wolfson <todd@twolfson.com>
2013-11-08 19:10:52 -0800

    Still not much room for comparison

--- demo/gif-generate.js
+++ demo/gif-generate.js
@@ -36,35 +36,35 @@ for (var i = 0; i < maxKey; i++) {
 var atob2 = require('atob');
 // Compare to canvas data
 // https://gist.github.com/borismus/1032746
-// var BASE64_MARKER = ';base64,';
+var BASE64_MARKER = ';base64,';
 
-// function convertDataURIToBinary(dataURI) {
-//   var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
-//   var base64 = dataURI.substring(base64Index);
-//   // var raw = window.atob(base64);
-//   var raw = atob2(base64);
-//   var rawLength = raw.length;
-//   var array = new Uint8ClampedArray(new ArrayBuffer(rawLength));
+function convertDataURIToBinary(dataURI) {
+  var base64Index = dataURI.indexOf(BASE64_MARKER) + BASE64_MARKER.length;
+  var base64 = dataURI.substring(base64Index);
+  // var raw = window.atob(base64);
+  var raw = atob2(base64);
+  var rawLength = raw.length;
+  var array = new Uint8ClampedArray(new ArrayBuffer(rawLength));
 
-//   for(i = 0; i < rawLength; i++) {
-//     array[i] = raw.charCodeAt(i);
-//   }
-//   return array;
-// }
+  for(i = 0; i < rawLength; i++) {
+    array[i] = raw.charCodeAt(i);
+  }
+  return array;
+}
 
 // http://stackoverflow.com/questions/16194908/how-can-i-create-a-canvas-imagedata-array-from-an-arraybuffer-representation-of
-function convertDataURIToBinary(dataURI) {
-  // uri to Base64
-  b64 = dataURI.slice(dataURI.indexOf(',')+1);
-  // to String
-  str = atob2(b64);
-  // to Array
-  arr = str.split('').map(function (e) {return e.charCodeAt(0);});
-  // to Uint8ClampedArray
-  u = new Uint8ClampedArray(arr); // [255, 56, 201, 8]
+// function convertDataURIToBinary(dataURI) {
+//   // uri to Base64
+//   b64 = dataURI.slice(dataURI.indexOf(',')+1);
+//   // to String
+//   str = atob2(b64);
+//   // to Array
+//   arr = str.split('').map(function (e) {return e.charCodeAt(0);});
+//   // to Uint8ClampedArray
+//   u = new Uint8ClampedArray(arr); // [255, 56, 201, 8]
 
-  return u;
-}
+//   return u;
+// }
 console.log(convertDataURIToBinary(canvas.toDataURL()));
 // convertDataURIToBinary(canvas.toDataURL());
 
