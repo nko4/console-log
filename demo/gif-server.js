6a77d7e314e7e36b3888d44e5c90af2288dada9b
Todd Wolfson <todd@twolfson.com>
2013-11-09 20:27:21 -0800

    Added a lot more timing statements

--- demo/gif-server.js
+++ demo/gif-server.js
@@ -37,12 +37,12 @@ module.exports = function gifRepl (port) {
 
   function writeTextToConnections(req, res) {
     // Parse in the body (up to 1mb)
-    console.log('Parsing body');
+    console.log('BODY-PARSE: Parsing body');
     getRawBody(req, {
       expected: req.headers['content-length'],
       limit: 1 * 1024 * 1024 // 1 mb
     }, function (err, buffer) {
-      console.log('Body parsed');
+      console.log('BODY-PARSE: Body parsed');
       // If there was an error (e.g. bad length, over length), respond poorly
       if (err) {
         res.writeHead(500, {
@@ -59,7 +59,9 @@ module.exports = function gifRepl (port) {
       // Generate a new GIF to encode
       var gif = new GifPerformance();
 
+      console.log('IMAGE-CREATE: Creating image');
       gif.getTextFrameData(text, function receiveTextFrameData (err, imageData) {
+        console.log('IMAGE-CREATE: Writing image data');
         function writeToFirstConnections(buff) {
           firstConnections.forEach(function writeToFirstConnection (conn) {
             conn.res.write(buff);
@@ -78,22 +80,30 @@ module.exports = function gifRepl (port) {
         }
 
         // Process the image (addFrame#1)
+        console.log('ANALYZE: Analyzing image');
         gif.analyzeImage(imageData);
+        console.log('ANALYZE: Image analyzed');
 
         // Write out the image info for the first connections (addFrame#2)
+        console.log('FIRST-INFO: Writing image info to first connections');
         gif.writeImageInfo();
         gif.flushData();
+        console.log('FIRST-INFO: First conections completed');
 
         // Write out the image info for the second connections (addFrame#2)
+        console.log('SECOND-INFO: Writing image info to second connections');
         gif.removeListener('data', writeToFirstConnections);
         gif.on('data', writeToSecondConnections);
         gif.writeImageInfo();
         gif.flushData();
+        console.log('SECOND-INFO: Second conections completed');
 
         // Write out the image itself for all connections (addFrame#3)
+        console.log('IMAGE-ALL: Writing image to all connections');
         gif.on('data', writeToFirstConnections);
         gif.outputImage();
         gif.flushData();
+        console.log('IMAGE-ALL: All connections completed');
 
         // Clean up event listeners
         gif.removeAllListeners();
