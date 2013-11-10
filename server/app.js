6eb1664dd7de5b748fe22ed83e23ab369462e7f2
Todd Wolfson <todd@twolfson.com>
2013-11-09 21:23:08 -0800

    Very basic server setup

--- server/app.js
+++ server/app.js
@@ -1,24 +1,34 @@
-var http = require('http');
+var express = require('express');
 
 var routes = require('./routes');
 
 // TODO: Debug single quotes in URLs
 
-module.exports = function gifRepl (port) {
+module.exports = function gifServer (port) {
 
   // Keep track of array of connections
   var firstConnections = [];
   var secondConnections = [];
 
-  var app = http.createServer(function (req, res) {
+  // Start a server that runs on jade
+  var app = express();
+
+  // Host static files
+  app.use('/public', express['static'](__dirname + '/public'));
+
+  // Host homepage
+  app.get('/', routes.index);
+
+  // Server logic
+  app.use(function saveConnections (req, res, next) {
     req.firstConnections = firstConnections;
     req.secondConnections = secondConnections;
-    if (req.method === 'POST') {
-      routes.writeToImage(req, res);
-    } else {
-      routes.openImage(req, res);
-    }
   });
+  app.get('/image.gif', routes.openImage);
+  app.post('/image', routes.writeToImage);
+
+  // Host 404 page
+  app.all('*', routes[404]);
 
   app.listen(port);
   console.log('gifsockets server is listening at http://127.0.0.1:' + port + '/');
--- server/routes/index.js
+++ server/routes/index.js
@@ -12,3 +12,22 @@ exports.openImage = function openImage (req, res) {
 };
 
 exports.writeToImage = require('./writeToImage');
+
+// Render some jade into memory
+var fs = require('fs');
+var jade = require('jade');
+function renderView(filepath, locals) {
+  var file = fs.readFileSync(filepath, 'utf8');
+  return jade.render(file, locals);
+}
+
+var indexHtml = renderView(__dirname + '/../../views/index.jade', {});
+exports.index = function (req, res) {
+  res.send(indexHtml);
+};
+
+var pageNotFoundHtml = renderView(__dirname + '/../../views/404.jade', {});
+exports[404] = function (req, res) {
+  res.status(404);
+  res.send(pageNotFoundHtml);
+};
