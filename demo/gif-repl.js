ba1965838eb2007dcc4de56168e9081ffb760ec9
Todd Wolfson <todd@twolfson.com>
2013-11-09 16:29:32 -0800

    Got repl working. Fuck yesss.

--- demo/gif-repl.js
+++ demo/gif-repl.js
@@ -1,42 +1,57 @@
+var fs = require('fs');
+var http = require('http');
 var readline = require('readline');
 
-if (module.parent === null) {
-  // Keep track of array of connections
-  // When we get a readline, genreate a text frame for each connection
+var async = require('async');
 
+var GifDuplex = require('../lib/gif-duplex');
+
+module.exports = function gifRepl (port) {
+
+  // Keep track of array of connections
+  var connections = [];
+
+
+  var app = http.createServer(function (req, res) {
+    res.writeHead(200, {
+      'connection': 'keep-alive',
+      'content-type': 'image/gif',
+      'transfer-encoding': 'chunked'
+    });
+    var gifDuplex = new GifDuplex();
+
+    gifDuplex.on('data', function (buff) {
+      res.write(buff);
+    });
+
+    gifDuplex.on('end', function () {
+      res.end();
+    });
+
+    connections.push(gifDuplex);
+  });
+
+  app.listen(port);
+  console.log('gifsockets server is listening at http://127.0.0.1:' + port + '/');
+
+  // When we get a readline, genreate a text frame for each connection
   var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
   });
 
   function prompt() {
-    rl.question('Enter text> ', function (answer) {
-      console.log('Echo: ' + answer);
+    rl.question('Enter text> ', function (text) {
+      console.log('Outputting: ' + text);
+      connections.forEach(function writeToResponse (gifDuplex) {
+        gifDuplex.writeTextFrame(text);
+      });
       prompt();
     });
   }
   prompt();
+};
 
-  // TODO: Create streaming server via duplex (scrapping gif-generate)
-  // var gifDuplex = new GifDuplex();
-
-  // async.eachSeries([
-  //   'Hello',
-  //   'Hello World',
-  //   'Hello World!',
-  //   'Hello World!!!'
-  // ], function addTextFrame (text, cb) {
-  //   gifDuplex.writeTextFrame(text, cb);
-  // }, function handleTextError (err) {
-  //   if (err) {
-  //     throw err;
-  //   }
-
-  //   // Complete the gif
-  //   gifDuplex.finish();
-  // });
-
-  // gifDuplex.on('data', function (buff) {
-  //   stream.write(buff);
-  // });
+if (module.parent === null) {
+  module.exports(7000);
 }
