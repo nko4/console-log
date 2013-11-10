bfaed7b65b68eefcc258642d361e16832421585c
Todd Wolfson <todd@twolfson.com>
2013-11-09 16:14:24 -0800

    Breaking up false gif-repl into gif-duplex

---
+++ demo/gif-duplex.js
@@ -0,0 +1,24 @@
+if (module.parent === null) {
+  var stream = fs.createWriteStream(__dirname + '/test-duplex.gif');
+  var gifDuplex = new GifDuplex();
+
+  async.eachSeries([
+    'Hello',
+    'Hello World',
+    'Hello World!',
+    'Hello World!!!'
+  ], function addTextFrame (text, cb) {
+    gifDuplex.writeTextFrame(text, cb);
+  }, function handleTextError (err) {
+    if (err) {
+      throw err;
+    }
+
+    // Complete the gif
+    gifDuplex.finish();
+  });
+
+  gifDuplex.on('data', function (buff) {
+    stream.write(buff);
+  });
+}
